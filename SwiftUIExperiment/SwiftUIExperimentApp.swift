e053832b6676e2be58fbc8d34d308f7c75273894
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-11-22 17:06:08 -0800

    Initial Commit

---
+++ SwiftUIExperiment/SwiftUIExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExperimentApp.swift
+//  SwiftUIExperiment
+//
+//  Created by Alphonso Sensley II on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
